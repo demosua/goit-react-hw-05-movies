305d2a29077d40e02a93b40c7b9c31ca42dd1f11
Dmytro Tkachuk <68943595+demosua@users.noreply.github.com>
2023-05-17 08:39:00 +0300

    browser basename

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const theme ={}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
